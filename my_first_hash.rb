d4013ad2ddf79ac95159b849004c4de563bfcbb9
Arif Cagirici <acagirici23@gmail.com>
2020-02-02 00:14:45 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -8,13 +8,8 @@ the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil painting
 end
 
 def retrieval
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
-shipping_manifest{"oil paintings"}
-
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+shipping_manifest["oil paintings"]
 end
 
 def adding
